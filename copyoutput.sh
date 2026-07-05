db499f0baaa874b3015563d658fa4dbded827296
coolight <2465045051@qq.com>
2026-07-05 10:13:33 +0800

    --

--- copyoutput.sh
+++ copyoutput.sh
@@ -1,5 +1,5 @@
 ls -lh ./output/iOS/arm64/*.dylib
-cp ./output/iOS/arm64/*.dylib /Users/coolight/0Acoolight/program/flutter/mymusic/resource/ffmpeg/ios/
+cp ./output/iOS/arm64/*.dylib /Users/coolight/0Acoolight/program/flutter/mymusic/resource/libs/ios/lib/
 
 ls -lh ./output/Darwin/arm64/*.dylib
-cp -r ./output/Darwin/arm64/*.dylib  /Users/coolight/0Acoolight/program/flutter/mymusic/resource/ffmpeg/macos/
+cp -r ./output/Darwin/arm64/*.dylib  /Users/coolight/0Acoolight/program/flutter/mymusic/resource/libs/macos/lib
